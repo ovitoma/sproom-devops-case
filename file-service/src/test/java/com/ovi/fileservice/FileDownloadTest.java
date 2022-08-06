beda47d23cb6bfdba377277f78e69ca6f99042bc
Ovidiu Toma <ovidiu.toma@gmail.com>
2022-08-06 21:43:32 +0300

    splitting workflows

---
+++ file-service/src/test/java/com/ovi/fileservice/FileDownloadTest.java
@@ -0,0 +1,29 @@
+package com.ovi.fileservice;
+
+import com.ovi.fileservice.metrics.MicrometerRegistry;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.RequestBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@ExtendWith({SpringExtension.class})
+@WebMvcTest({FileDownloadController.class, FileMonitor.class, MicrometerRegistry.class, RandomFile.class})
+public class FileDownloadTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    void fileDownload() throws Exception {
+        RequestBuilder rq = MockMvcRequestBuilders.get("/randomFile");
+        MvcResult rs = mockMvc.perform(rq).andReturn();
+        assertEquals(200, rs.getResponse().getStatus());
+    }
+}
--- file-service/src/test/java/com/ovi/fileservice/FileServiceApplicationTests.java
+++ file-service/src/test/java/com/ovi/fileservice/FileServiceApplicationTests.java
@@ -1,13 +1,20 @@
 package com.ovi.fileservice;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 class FileServiceApplicationTests {
 
+	@Autowired
+	private FileDownloadController fileDownloadController;
+
 	@Test
 	void contextLoads() {
+		assertThat(fileDownloadController).isNotNull();
 	}
 
 }
